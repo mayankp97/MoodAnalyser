bb6b2d69881354ff05f10182910d7c6078f5260d
Mayank Purohit <mayankpurohit2012@gmail.com>
2020-10-09 23:36:04 +0530

    [Mayank] Add Unit tests for parameterized constructor using reflections

--- MoodAnalyserTests/MoodAnalyserFactoryTests.cs
+++ MoodAnalyserTests/MoodAnalyserFactoryTests.cs
@@ -10,7 +10,7 @@ namespace MoodAnalyserTests
     class MoodAnalyserFactoryTests
     {
         [Test]
-        public void Initialize_WhenPassedClassName_ReturnsMoodAnalyserObject()
+        public void Initialize_WhenPassedClassNameOnly_ReturnsMoodAnalyserObject()
         {
             //Arrange
             var className = "MoodAnalyser";
@@ -23,7 +23,7 @@ namespace MoodAnalyserTests
         }
 
         [Test]
-        public void Initialize_WhenPassedInvalidClassName_ReturnsMoodAnalysisException()
+        public void Initialize_WhenPassedInvalidClassNameOnly_ReturnsMoodAnalysisException()
         {
             //Arrange
             var className = "MoodAnalyse";
@@ -43,6 +43,32 @@ namespace MoodAnalyserTests
             //Assert
             Assert.That(() => MoodAnalyserFactory.Initialize(className,5), Throws.InstanceOf<MoodAnalysisException>());
             Assert.That(() => MoodAnalyserFactory.Initialize(className,5), Throws.Exception.Message.Contains("No such method").IgnoreCase);
+
+        }
+        [Test]
+        public void Initialize_WhenPassedClassNameWithParameters_ReturnsMoodAnalyserObject()
+        {
+            //Arrange
+            var className = "MoodAnalyser";
+            var parameter = "I am in Happy Mood";
+
+            //Act
+            var obj = MoodAnalyserFactory.Initialize(className,parameter);
+
+            //Assert
+            Assert.IsTrue(new MoodAnalyser.MoodAnalyser(parameter).Equals(obj));
+        }
+
+        [Test]
+        public void Initialize_WhenPassedInvalidClassNameWithParameters_ReturnsMoodAnalysisException()
+        {
+            //Arrange
+            var className = "MoodAnalyse";
+            var parameter = "I am in Happy Mood";
+
+            //Assert
+            Assert.That(() => MoodAnalyserFactory.Initialize(className,parameter), Throws.InstanceOf<MoodAnalysisException>());
+            Assert.That(() => MoodAnalyserFactory.Initialize(className,parameter), Throws.Exception.Message.Contains("No Such class").IgnoreCase);
 
         }
     }
